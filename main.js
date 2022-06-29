cae739a242b1dcc7723601e9ac4dfa920ebccd89
Sara Szwejkowska <saraszw@op.pl>
2022-06-30 00:13:53 +0200

    ex2 done

--- main.js
+++ main.js
@@ -18,14 +18,16 @@ const chars = 'ABCDEFGHIJK123456789';
 const btn = document.querySelector('button');
 const section = document.querySelector('section');
 
-const codesNumber = 1000;
-const charsNumber = 10;
+const codesNumber = 10;
+const charsNumber = 14;
 
 const codesGenerator = () => {
 	for (let i = 0; i < codesNumber; i++) {
 		let code = "";
 		for (let i = 0; i < charsNumber; i++) {
-			code += '1';
+            const index =Math.floor( Math.random() *20)
+        
+			code += chars[index];
 		}
 		const div = document.createElement('div');
 		div.textContent = code;
