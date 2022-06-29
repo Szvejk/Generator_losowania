836e46e06ba2a276a8d49dc2d3cbb65e29526d4d
Sara Szwejkowska <saraszw@op.pl>
2022-06-30 00:07:39 +0200

    other exercies

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
+// const btn = document.querySelector('button');
+// const div = document.querySelector('div');
+// const names = ["Jagienka", "Adam", "Adrian", "Bartosz", "Kamil", "Sara"];
+
+// const prefixs = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy, myślę...", "Szczerze uważam..."];
+
+// // console.log(names[index]);
+
+// const nameGenerator = () => {
+//     const indexName = Math.floor(Math.random() * names.length);
+//     const indexPrefix = Math.floor(Math.random() * prefixs.length);
+//     div.textContent = `${prefixs[indexPrefix]}że najlepsze będzie imię ${names[indexName]}`
+// }
+
+// btn.addEventListener('click', nameGenerator)
+
+const chars = 'ABCDEFGHIJK123456789';
 const btn = document.querySelector('button');
-const div = document.querySelector('div');
-const names = ["Jagienka", "Adam", "Adrian", "Bartosz", "Kamil", "Sara"];
+const section = document.querySelector('section');
 
-const prefixs = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy, myślę...", "Szczerze uważam..."];
+const codesNumber = 1000;
+const charsNumber = 10;
 
-// console.log(names[index]);
+const codesGenerator = () => {
+	for (let i = 0; i < codesNumber; i++) {
+		let code = "";
+		for (let i = 0; i < charsNumber; i++) {
+			code += '1';
+		}
+		const div = document.createElement('div');
+		div.textContent = code;
+		section.appendChild(div);
+	}
+};
 
-const nameGenerator = () => {
-    const indexName = Math.floor(Math.random() * names.length);
-    const indexPrefix = Math.floor(Math.random() * prefixs.length);
-    div.textContent = `${prefixs[indexPrefix]}że najlepsze będzie imię ${names[indexName]}`
-}
-
-
-
-btn.addEventListener('click', nameGenerator)
+btn.addEventListener('click', codesGenerator);
